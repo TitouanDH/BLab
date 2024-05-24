1672d13f9c6ded46200473ec2772b25b886bd4c0
Titouan_portable <titouan.dhalluin@al-enterprise.com>
2024-05-24 13:41:04 +0200

    main branch ip modif

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -5,7 +5,7 @@ const csrfToken = document.cookie.match(/csrftoken=([^;]+)/)[1];
 
 // Set up Axios instance with default configuration
 const api = axios.create({
-  baseURL: 'https://127.0.0.1/api/', // Adjust the base URL as needed
+  baseURL: 'https://10.69.145.176/api/', // Adjust the base URL as needed
   headers: {
     'X-CSRFToken': csrfToken, // Include CSRF token in every request
     'Authorization': `Token ${localStorage.getItem('token')}` // Include authentication token in every request
