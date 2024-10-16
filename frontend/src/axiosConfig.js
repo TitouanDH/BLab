90a7076c55683bbbe8804200f0f528558e4a991b
Titouan_portable <titouan.dhalluin@al-enterprise.com>
2024-10-16 14:52:51 +0200

    ip change

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -14,7 +14,7 @@ if (!csrfToken) {
 
 // Set up Axios instance with default configuration
 const api = axios.create({
-  baseURL: 'https://127.0.0.1/api/', // Adjust the base URL as needed
+  baseURL: 'https://10.69.144.180/api/', // Adjust the base URL as needed
   headers: {
     'X-CSRFToken': csrfToken, // Include CSRF token in every request
     'Authorization': `Token ${localStorage.getItem('token')}` // Include authentication token in every request
