8c5e45ac5912713a004bb2cd2850f86bca6f078a
Titouan_portable <titouan.dhalluin@al-enterprise.com>
2025-02-25 10:54:34 +0100

    database change

--- api/backend/settings.py
+++ api/backend/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = "wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blab_db',
         'USER': 'root',
         'PASSWORD': 'Letacla01*',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -14,7 +14,7 @@ if (!csrfToken) {
 
 // Set up Axios instance with default configuration
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',  // Django's default development port
+  baseURL: 'https://10.69.144.180/api/',  // API HTTPS
   headers: {
     'X-CSRFToken': csrfToken,
     'Authorization': `Token ${localStorage.getItem('token')}`
